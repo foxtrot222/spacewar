1d44722e8957e0967c2bbead35f8b081f8ad0ff9
ROHAN MACHA <98206744+userg3709@users.noreply.github.com>
2026-08-08 15:15:01 +0530

    Update global.gd

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
-const gravity\_strength: float = 500000.0
-const kill\_radius: float = 40.0
+const gravity_strength: float = 500000.0
+const kill_radius: float = 40.0
 
 # Called when the node enters the scene tree for the first time.
 
-func \_ready() -> void:
-Global.gravity\_well = self
+func _ready() -> void:
+Global.gravity_well = self
 pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
-func \_process(delta: float) -> void:
+func _process(delta: float) -> void:
 pass
