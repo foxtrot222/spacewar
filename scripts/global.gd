a4ea5ee1a357988ab410189d5481f91751edf156
ROHAN MACHA <98206744+userg3709@users.noreply.github.com>
2026-08-08 15:15:34 +0530

    fixed global.gd after merge

--- scripts/global.gd
+++ scripts/global.gd
@@ -6,10 +6,10 @@ const kill_radius: float = 40.0
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
-Global.gravity_well = self
+	Global.gravity_well = self
 pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _process(delta: float) -> void:
-pass
+	pass
