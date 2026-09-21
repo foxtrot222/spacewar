63c2c1f7149db022624c78b22b450d8409d3277b
PheonixKB <kavyabhatiya3019@gmail.com>
2026-09-21 23:06:12 +0530

    Collison of lasers with missiles and bullets

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -61,17 +61,20 @@ func _physics_process(delta: float) -> void:
 				collider.take_damage(1)  # 1 damage per 10ms
 			damage_timer = 0.0
 		
-		# Check if it's a missile - laser destroys missile
+		# Check what we hit and destroy accordingly
 		var collider = get_collider()
-		if collider and collider.name == "Missile":
-			collider.queue_free()
-		# Check if it's a bullet - laser destroys bullet
-		elif collider and collider.name == "Bullet":
-			collider.queue_free()
-		# Check if it's another laser - lasers destroy each other
-		elif collider and collider.name == "Laser":
-			collider.queue_free()
-			# This laser will be handled by the other laser's collision
+		if collider:
+			print("Laser hit: ", collider.name, " (type: ", collider.get_class(), ")")
+			# Check if it's a missile - laser destroys missile
+			if collider.name == "Missile":
+				collider.queue_free()
+			# Check if it's a bullet - laser destroys bullet
+			elif collider.name == "Bullet":
+				collider.queue_free()
+			# Check if it's another laser - lasers destroy each other
+			elif collider.name == "Laser":
+				collider.queue_free()
+				# This laser will be handled by the other laser's collision
 	else:
 		laser_end_position = laser_target
 		damage_timer = 0.0
