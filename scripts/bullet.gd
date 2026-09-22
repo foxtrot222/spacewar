9074efd076ed366f5f0e8c13b232462916859ecf
Tirth Kavathiya <tirthkavathiya@gmail.com>
2026-09-22 13:10:46 +0530

    feat(explosions): add missile and bullet explosions

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -27,6 +27,7 @@ func _on_body_entered(body: Node2D) -> void:
 		var damage := BULLET_DAMAGE
 		print("Bullet hit Player %s! Damage: %s" % [int(body.player_prefix), damage])
 		body.take_damage(damage)
+		Global.explosion.set_explosion(position, shooter.color, "BulletExplode")
 		queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
--- scripts/missile.gd
+++ scripts/missile.gd
@@ -29,6 +29,7 @@ func _on_body_entered(body: Node2D) -> void:
 			return
 		body.take_damage(MISSILE_DAMAGE)
 		print("Missile hit Player %s! Damage: %s" % [int(body.player_prefix), MISSILE_DAMAGE])
+		Global.explosion.set_explosion(position, shooter.color, "MissileExplode")
 		queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
